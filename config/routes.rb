7bcf04b61ddaa7eb34643ac4602a263291f96288
weezer924 <weezer970@gmail.com>
2021-03-05 12:50:17 +0900

    create welcome index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   resources :microposts
   resources :users
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "users#index"
+  root "welcome#index"
 end
