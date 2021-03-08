b036a2123d3442fd7cc5125bebe77715308d2e26
weezer924 <weezer970@gmail.com>
2021-03-08 12:19:19 +0900

    add articles controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_023756) do
+ActiveRecord::Schema.define(version: 2021_03_05_052644) do
 
-  create_table "microposts", force: :cascade do |t|
+  create_table "articles", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "microposts", charset: "utf8mb4", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
